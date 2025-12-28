1a19374f9dcf5452a24036d784f83e27bdff2306
Navth <am.en.u4cse22039@am.students.amrita.edu>
2025-12-28 15:48:30 +0530

    railway dep

--- server/index.ts
+++ server/index.ts
@@ -67,7 +67,7 @@ app.use((req, res, next) => {
   }
 
   const PORT = Number(process.env.PORT ?? 5000);
-  httpServer.listen(PORT, '127.0.0.1', () => {
-    console.log(`Server running on http://127.0.0.1:${PORT}`);
+  httpServer.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
   });
 })();
